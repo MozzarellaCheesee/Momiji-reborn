645fecf098de94bcf31d3056ded2570f57a05206
Your Reimu <106019215+AstolfoOwO@users.noreply.github.com>
2023-11-18 20:09:49 +0700

    .

--- extensions/events/on_error.py
+++ extensions/events/on_error.py
@@ -129,5 +129,5 @@ class OnErrors(BaseCog):
         )
 
 
-# def setup(client: commands.InteractionBot):
-#     client.add_cog(OnErrors(client))
+def setup(client: commands.InteractionBot):
+    client.add_cog(OnErrors(client))
