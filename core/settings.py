63206de1fc0127adf0ff6a8a3638f916eeba0e25
mozzarella <Code, planning, and automation Security Integrations Archives Emails You have a single verified email associated with your GitHub account. Add an additional verified email address in case you lose access to your primary email.  Danilvasilkov59@gmail.com>
2024-12-14 03:31:27 +0700

    pre-update #2

--- core/settings.py
+++ core/settings.py
@@ -4,26 +4,26 @@ config = dotenv_values()
 DB_URL="mysql://p527749_astolfo_owo:lI7xK0dC0c@185.105.110.5:3306/p527749_momiji_db"
 
 commands_ = {
-    # "extensions.commands.accounts",
-    # "extensions.commands.developer",
+    "extensions.commands.accounts",
+    "extensions.commands.developer",
     "extensions.commands.economy",
-    # "extensions.commands.family",
-    # "extensions.commands.gaiety",
-    # "extensions.commands.info",
-    # "extensions.commands.moderation",
-    # "extensions.commands.profile",
-    # "extensions.commands.rating",
-    # "extensions.commands.roleplay",
-    # "extensions.commands.server_settings",
-    # "extensions.commands.user",
-    # "extensions.commands.utilits",
+    "extensions.commands.family",
+    "extensions.commands.gaiety",
+    "extensions.commands.info",
+    "extensions.commands.moderation",
+    "extensions.commands.profile",
+    "extensions.commands.rating",
+    "extensions.commands.roleplay",
+    "extensions.commands.server_settings",
+    "extensions.commands.user",
+    "extensions.commands.utilits",
     "extensions.commands.admin"
 }
 events = {
-    # "extensions.events.levels",
-    # "extensions.events.on_error",
-    # "extensions.events.on_guild_join",
-    # "extensions.events.on_guild_remove",
-    # "extensions.events.private_voices",
+    "extensions.events.levels",
+    "extensions.events.on_error",
+    "extensions.events.on_guild_join",
+    "extensions.events.on_guild_remove",
+    "extensions.events.private_voices",
     "extensions.events.on_ready",
 }
